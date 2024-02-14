01f8db6f25ed1cf7feec1a9b83c05b4dccfcbe22
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-02-14 11:04:27 +0100

    Add solution for K Items With the Maximum Sum

--- camp/leetcode/k-items-with-the-maximum-sum.py
+++ camp/leetcode/k-items-with-the-maximum-sum.py
@@ -2,10 +2,10 @@ class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
         
         leftP =k -(numOnes +numZeros)
-        if leftP <=0:
+        if leftP <=0: #if all place in k's are allocated by 1 and 0
             return min(k, numOnes)
 
-        return numOnes -leftP
+        return numOnes -leftP #if there is place left from 0 & 1, we have to put -1 inorder to fill the bag
         
         
         
