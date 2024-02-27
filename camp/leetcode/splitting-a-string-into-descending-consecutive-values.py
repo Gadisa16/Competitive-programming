99f6ac0ac9f8db81ec4c330f400eeed7d8d46534
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-02-27 10:50:22 +0300

    Add solution for Splitting a String Into Descending Consecutive Values

--- camp/leetcode/splitting-a-string-into-descending-consecutive-values.py
+++ camp/leetcode/splitting-a-string-into-descending-consecutive-values.py
@@ -1,18 +1,16 @@
 class Solution:
-    def helper(self,s,prev,idx):
-        if(idx == len(s) and prev != s ):
-            return True 
-        c = ""
-        for i in range(idx,len(s)):
-            c += s[i]
-            if(prev != ""):
-                if(int(prev) - int(c) == 1 ):
-                    if(self.helper(s,c,i+1)): 
-                        return True 
-            else:
-                if(self.helper(s,c,i+1)):
-                    return True 
-        return False 
+    def splitString(self, s: str) -> bool:
+        def dfs(prev, indx):
+            if indx==len(s):
+                return True
+
+            for j in range(indx, len(s)):
+                curv =int(s[indx: j+1])
+                if curv- prev==-1 and dfs(curv, j+1):
+                    return True
+            return False
         
-    def splitString(self, s: str) -> bool:   
-        return self.helper(s,"",0)
+        for i in range(len(s)-1):
+            val =int(s[:i+1])
+            if dfs(val, i+1): return True
+        return False
